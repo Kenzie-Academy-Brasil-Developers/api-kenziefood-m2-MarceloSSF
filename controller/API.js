d2d492a220173f5e618bfb1a0ccd91815b750731
Marcelo Soares <soaresmarcelo94@gmail.com>
2022-05-18 17:04:34 -0300

    update: requisicoes /cart Api

--- controller/API.js
+++ controller/API.js
@@ -56,4 +56,53 @@ export class Api {
 
             return token;
     }
+
+    static async buscarItensCarrinho() {
+
+        const URLPEGARCART = 'https://api-kenzie-food.herokuapp.com/cart';
+
+        const response = await fetch(URLPEGARCART, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(error => error)
+    
+        return response;
+    }
+
+    static async colocarItemCarrinho(data) {
+        const URLADDITEMCARRINHO = 'https://api-kenzie-food.herokuapp.com/cart/add';
+        const response = await fetch(URLCRIARUSUARIO, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(error => error)
+
+        return response;
+    }
+
+    static async deletarItemCarrinho(id) {
+        const URLDELETARCARRINHO = `https://api-kenzie-food.herokuapp.com/cart/remove/:${product_id}`;
+
+        const response = await fetch(URLDELETARCARRINHO, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(error => error)
+        
+        return response;
+    }
 }
